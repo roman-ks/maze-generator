843207ba9223d937b5aef86903a5f7db82c8d0fe
Roman Ks <ks_roman@ukr.net>
2021-03-15 20:04:16 +0200

    Maze generators test only one solution

--- src/test/java/com/roman_ks/maze/generator/NaiveGeneratorTest.java
+++ src/test/java/com/roman_ks/maze/generator/NaiveGeneratorTest.java
@@ -1,14 +1,32 @@
 package com.roman_ks.maze.generator;
 
 import com.roman_ks.maze.generator.adjacency.RectAdjacencyMatrixGenerator;
+import com.roman_ks.maze.generator.builder.GeneratorBuilder;
+import com.roman_ks.maze.generator.builder.director.RectGeneratorDirector;
 import com.roman_ks.maze.generator.model.RectMaze;
 import com.roman_ks.maze.generator.selector.CenterEntranceSelector;
 import com.roman_ks.maze.generator.selector.RandomNodeSelector;
+import com.roman_ks.maze.generator.utils.MazeUtils;
 import com.roman_ks.maze.generator.visitor.ToStringVisitor;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 class NaiveGeneratorTest {
+
+    @Test
+    void generateMaze_oneSolution() {
+        int width = 20;
+        int height = 20;
+
+        var builder = new GeneratorBuilder(NaiveGenerator::new);
+        var director = new RectGeneratorDirector(builder);
+        var generator = director.edgesAndRandomNodeSelector(width, height, 1L);
+
+        var maze = generator.generateMaze();
+
+        Assertions.assertEquals(1, MazeUtils.countSolutions(maze));
+    }
 
     @Disabled
     @Test
